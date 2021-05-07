9aefdee699e65d88ab4ca988980db3e943ca4463
Grigor Azatian <grigorazatian@yahoo.com>
2021-05-07 11:47:21 -0700

    initial dev branch commit

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+//store gameboard as an array inside of a gameboard object  -   only need ONE, so make this a MODULE
+
+//store players in objects  -   need MULTIPLE, so make these with a FACTORY
+
 console.log("testing");
