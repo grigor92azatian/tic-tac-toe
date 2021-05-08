77e88f7bafd6cc72f3e4ed2d036efc5e1ec922aa
Grigor Azatian <grigorazatian@yahoo.com>
2021-05-07 21:36:50 -0700

    testing out factory functions for player objects, will revisit once second page design is complete

--- script.js
+++ script.js
@@ -2,4 +2,30 @@
 
 //store players in objects  -   need MULTIPLE, so make these with a FACTORY
 
-console.log("testing");
+
+
+let playButton = document.querySelector(".formButton");
+playButton.addEventListener("click",function(){
+    let player1Name = document.querySelector("#player1Name").value;
+    let player2Name = document.querySelector("#player2Name").value;
+    console.log({player1Name,player2Name});    
+});
+
+function playerFactory(name, piece){    //factory function
+    let score = 0;
+
+    return {
+        name,
+        piece,
+        score,
+        incrementScore: function(){
+            this.score++;
+        }
+    };
+}
+
+let newPlayer1 = playerFactory("Ara", "X");
+
+
+
+
