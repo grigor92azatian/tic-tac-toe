9c61c4127e3cca38b0f113186e47a115052f656c
Grigor Azatian <grigorazatian@yahoo.com>
2021-05-10 20:26:28 -0700

    backed up

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ let playButton = document.querySelector(".formButton");
 playButton.addEventListener("click",function(){
     let player1Name = document.querySelector("#player1Name").value;
     let player2Name = document.querySelector("#player2Name").value;
-    console.log({player1Name,player2Name});    
+    console.log({player1Name,player2Name});
+    gameBoardModule.createGrid();    
 });
 
-function playerFactory(name, piece){    //factory function
+function playerFactory(name, piece){    //factory function for creating a player object that will contain name, piece (x or o), and score
     let score = 0;
 
     return {
@@ -24,8 +25,40 @@ function playerFactory(name, piece){    //factory function
     };
 }
 
-let newPlayer1 = playerFactory("Ara", "X");
 
 
+//making the actual 3x3 grid gameboard
+let gameBoardModule = (function(){
+    //VARIABLES
+        let gridContainer = document.createElement("div");
+        let gameBoardArray = [];
+        let gameDisplay = document.querySelector(".gameDisplay");
 
-
+    //style container that will store the 3x3 grid
+        gridContainer.setAttribute("class", "gameBoard");
+        gridContainer.style.cssText = "border: 2px solid green;"+
+                                "width: 600px;"+
+                                "height: 600px;"+
+                                "display: flex;"+
+                                "flex-wrap: wrap;";
+    //create an array of 9 div elements that will be the tiles to the game board
+        for (let i=0;i<9;i++){
+            let gridTile = document.createElement("div");
+            // gridTile.classList.add("boardTile", tileClassName);     //use classList.add to add multiple classes to single element
+            gridTile.setAttribute("class", "boardTile");    //set shared class
+            gridTile.setAttribute("data-indexNumber", i);   //set unique data attribute
+            gridTile.style.cssText = "border: 1px solid black;"+
+                                    "height: 200px;"+
+                                    "width: 200px;";
+            gameBoardArray.push(gridTile);
+        }
+    //append the 9 tiles to the grid container and then append grid container to gameDisplay element
+        function createGrid(){
+        for(let i=0;i<gameBoardArray.length;i++){
+            gridContainer.append(gameBoardArray[i]);
+        }
+        gameDisplay.innerHTML = "";
+        gameDisplay.append(gridContainer);    
+        }
+        return {createGrid};
+})();
