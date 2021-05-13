9b2f066f0ba9f597a94e185cc649e7e39588a724
Grigor Azatian <grigorazatian@yahoo.com>
2021-05-12 20:01:10 -0700

    save changes

--- script.js
+++ script.js
@@ -15,40 +15,48 @@ let pageLayout = (function(){   //GENERATES BASIC LAYOUT OF PAGE...OTHER MODULES
 
     divArray[0].setAttribute("id", "title");
         divArray[0].innerHTML = "Title";
-        divArray[0].style.cssText = "height: 7%; width: 100%; text-align: center; border: 1px solid black;";
+        divArray[0].style.cssText = "height: 7%; width: 100%; text-align: center; font-size: 50px; font-weight: bold;";
+        //divArray[0].style.border = "1px solid black";
 
     divArray[1].setAttribute("id", "mainSection");
         //divArray[1].innerHTML = "Main Section";
-        divArray[1].style.cssText = "display: flex; flex-direction: column; height: 93%; width: 100%; border: 1px solid black;";
+        divArray[1].style.cssText = "display: flex; flex-direction: column; height: 93%; width: 100%;";
         divArray[1].style.border = "1px solid black;";
+        divArray[1].style.border = "1px solid black";
 
     divArray[2].setAttribute("id", "gameInfo");
         //divArray[2].innerHTML = "Game Info";
-        divArray[2].style.cssText = "display: flex; flex-direction: row; height: 25%; border: 1px solid black;";
+        divArray[2].style.cssText = "display: flex; flex-direction: row; height: 25%;";
+        divArray[2].style.border = "1px solid black";
 
     divArray[3].setAttribute("id", "gameBoardSection");
         //divArray[3].innerHTML = "Game Board Section";
-        divArray[3].style.cssText = "display: flex; height: 75%; border: 1px solid black; align-items: center; justify-content: center;";
+        divArray[3].style.cssText = "display: flex; height: 75%; align-items: center; justify-content: center;";
+        divArray[3].style.border = "1px solid black";
 
     divArray[4].setAttribute("id", "player1Info");
-        divArray[4].innerHTML = "Player 1 Info";
-        divArray[4].style.cssText = "width: 25%; height: 100%; border: 1px solid black;";
+        //divArray[4].innerHTML = "Player 1 Info";
+        divArray[4].style.cssText = "width: 25%; height: 100%;";
+        divArray[4].style.border = "1px solid black";
 
     divArray[5].setAttribute("id", "player2Info");
-        divArray[5].innerHTML = "Player 2 Info";
-        divArray[5].style.cssText = "width: 25%; height: 100%; border: 1px solid black;";
+        //divArray[5].innerHTML = "Player 2 Info";
+        divArray[5].style.cssText = "width: 25%; height: 100%;";
+        divArray[5].style.border = "1px solid black";
 
     divArray[6].setAttribute("id", "currentRoundDisplay");
         divArray[6].innerHTML = "Current Round Display";
-        divArray[6].style.cssText = "width: 50%; height: 100%; border: 1px solid black;";
+        divArray[6].style.cssText = "width: 50%; height: 100%;";
+        divArray[6].style.border = "1px solid black";
 
     divArray[7].setAttribute("id", "gameBoard");
-        divArray[7].innerHTML = "GameBoard";
-        divArray[7].style.cssText = "width: 600px; height: 600px; border: 1px solid black;"
+        //divArray[7].innerHTML = "GameBoard";
+        divArray[7].style.cssText = "width: 600px; height: 600px;";
+        //divArray[7].style.border = "1px solid black";
 
 
     //--------------------------CREATE PAGE LAYOUT BY APPENDING THE DIVS TO EACHOTHER---------------------------------
-    function appendAll(){
+    function createPage(){
         divArray[3].appendChild(divArray[7]);   //append game board to game board section
         divArray[2].appendChild(divArray[4]);   //append player 1 and 2 info and round display to game info
         divArray[2].appendChild(divArray[6]);
@@ -58,8 +66,24 @@ let pageLayout = (function(){   //GENERATES BASIC LAYOUT OF PAGE...OTHER MODULES
         htmlBody.appendChild(divArray[0]);      //append title to body
         htmlBody.appendChild(divArray[1]);      //append main section to body           
     }
+
+    function fillTitle(contents){
+        divArray[0].innerHTML = contents;
+    }
+    function fillPlayer1Info(contents){
+        divArray[4].appendChild(contents);
+    }
+    function fillPlayer2Info(contents){
+        divArray[5].appendChild(contents);
+    }
+    function fillCurrentRoundDisplay(contents){
+        divArray[6].appendChild(contents);
+    }
+    function fillGameBoard(contents){
+        divArray[7].appendChild(contents);
+    }
  
-    return {appendAll};
+    return {createPage, fillTitle, fillCurrentRoundDisplay, fillPlayer1Info, fillPlayer2Info, fillGameBoard};
 })();
 
 
@@ -72,8 +96,8 @@ let gameDisplay = (function(){
 
 let gameBoard = (function(){    //making the actual 3x3 grid gameboard
     //VARIABLES
-        let gameBoard = document.querySelector(".gameBoard");
-        let mainSection = document.querySelector(".mainSection");
+        // let gameBoard = document.querySelector(".gameBoard");
+        // let mainSection = document.querySelector(".mainSection");
 
         let gridContainer = document.createElement("div");
         let gameBoardArray = [];
@@ -120,8 +144,7 @@ let gameBoard = (function(){    //making the actual 3x3 grid gameboard
             for(let i=0;i<gameBoardArray.length;i++){
                 gridContainer.append(gameBoardArray[i]);
             }
-            mainSection.innerHTML = "";
-            gameBoard.append(gridContainer);    
+            return gridContainer;
         }
 
         return {createGrid};
@@ -129,46 +152,61 @@ let gameBoard = (function(){    //making the actual 3x3 grid gameboard
 
 
 let playerFactory = function(name, piece){    //factory function for creating a player object that will contain name, piece (x or o), and score
+    //-----------------------------------------------------VARIABLES-----------------------------------------------------------
     let score = 0;
+    let playerName = name;
+    let playerPiece = piece;
+    let playerInfoArray = (function(){
+        let playerInfo = [];
+        for(let i=0;i<3;i++){
+            let newDiv = document.createElement("div");
+                newDiv.setAttribute("class", "playerInfo");
+                newDiv.style.textAlign = "center";
+            playerInfo.push(newDiv);
+        }
+        playerInfo[0].innerHTML = playerName;
+        playerInfo[0].setAttribute("class", "playerName");
+        playerInfo[1].innerHTML = playerPiece;
+        playerInfo[1].setAttribute("class", "playerPiece");
+        playerInfo[2].innerHTML = score;
+        playerInfo[2].setAttribute("class", "playerScore");        
+
+        return playerInfo;
+    })();
+    let container = (function(){
+        let containerDiv = document.createElement("div");
+        containerDiv.setAttribute("class", "playerInfoContainer");
+        containerDiv.style.cssText = "width: 100%; height: 100%; display: flex; flex-direction: column;"+
+                                "align-items: center; justify-content: space-around; font-size: 35px;";
+        return containerDiv;  
+    })();
+
+//-----------------------------------------------------FUNCTIONS-----------------------------------------------------------
+    let fillInfoContainer = function(){
+        for(let i=0;i<playerInfoArray.length; i++){
+            container.appendChild(playerInfoArray[i]);
+        }
+        return container;
+    };
+
+    let incrementScore = function(){
+        score++;
+        playerInfoArray[2].innerHTML = score;
+    };
 
     return {
-        name,
-        piece,
-        score,
-        incrementScore: function(){
-            this.score++;
-        },
-        createPlayerInfoDiv: function(){
-            let containerDiv = document.createElement("div");
-                containerDiv.setAttribute("class", "playerInfoContainer");
-                containerDiv.style.cssText = "width: 25%; height: 100%; display: flex; flex-direction: column;"+
-                                                "align-items: center; justify-content: space-between;";
-            let playerInfo = [];
-            for(let i=0;i<3;i++){
-                let newDiv = document.createElement("div");
-                    newDiv.setAttribute("class", "playerInfo");
-                    newDiv.style.textAlign = "center";
-                playerInfo.push(newDiv);
-            }
-            playerInfo[0].innerHTML = this.name;
-            playerInfo[1].innerHTML = this.piece;
-            playerInfo[2].innerHTML = this.score;
-
-            playerInfo[0].setAttribute("class", "playerName");
-            playerInfo[1].setAttribute("class", "playerPiece");
-            playerInfo[2].setAttribute("class", "playerScore");
-
-            for(let i=0;i<playerInfo.length; i++){
-                containerDiv.appendChild(playerInfo[i]);
-            }
-            return containerDiv;
-        }
+        incrementScore,
+        fillInfoContainer
     };
 };
 
 
+let player1 = playerFactory("Greg", "X");
+let player2 = playerFactory("John", "O");
 
 
-
-pageLayout.appendAll();
-
+pageLayout.createPage();
+pageLayout.fillTitle("Tic Tac Toe");
+pageLayout.fillGameBoard(gameBoard.createGrid());
+pageLayout.fillPlayer1Info(player1.fillInfoContainer());
+pageLayout.fillPlayer2Info(player2.fillInfoContainer());
