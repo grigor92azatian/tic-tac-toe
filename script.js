35c46f93bc2d68f890c52dfb6e452872e2443c02
Grigor Azatian <grigorazatian@yahoo.com>
2021-07-12 15:32:58 -0700

    extended testing to 3 tiles. Next, I need to figure out how to condense the code so that I am writing only one function for all tiles

--- script.js
+++ script.js
@@ -2,21 +2,39 @@
 
 //variable that stores an int, if int is odd, fill box with "X", if int is even, fill box with "O"
 
-let gameBoardTile  = document.querySelector(".box");
-
+let gameBoardTile1 = document.querySelector(".box1");
+let gameBoardTile2 = document.querySelector(".box2");
+let gameBoardTile3 = document.querySelector(".box3");
 
 let xOrO = 1;
-gameBoardTile.addEventListener("click", function(){
-    gameBoardTile.style.cssText = "font-size: 350px; text-align: center;";
-    if(xOrO%2===0){
-        gameBoardTile.innerHTML = "O";
+let fillBox1WithXorO = function(){
+    gameBoardTile1.style.cssText = "font-size: 350px; text-align: center;";
+if(gameBoardTile1.innerHTML === ""){    if(xOrO%2===0){
+        gameBoardTile1.innerHTML = "O";
     }else{
-        gameBoardTile.innerHTML = "X";
+        gameBoardTile1.innerHTML = "X";
+    }
+    xOrO++;}
+};
+let fillBox2WithXorO = function(){
+    gameBoardTile2.style.cssText = "font-size: 350px; text-align: center;";
+    if(xOrO%2===0){
+        gameBoardTile2.innerHTML = "O";
+    }else{
+        gameBoardTile2.innerHTML = "X";
     }
     xOrO++;
-});
+};
+let fillBox3WithXorO = function(){
+    gameBoardTile3.style.cssText = "font-size: 350px; text-align: center;";
+    if(xOrO%2===0){
+        gameBoardTile3.innerHTML = "O";
+    }else{
+        gameBoardTile3.innerHTML = "X";
+    }
+    xOrO++;
+};
 
-
-
-
-
+gameBoardTile1.addEventListener("click", fillBox1WithXorO);
+gameBoardTile2.addEventListener("click", fillBox2WithXorO);
+gameBoardTile3.addEventListener("click", fillBox3WithXorO);
