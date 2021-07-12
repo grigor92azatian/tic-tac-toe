e2e3826cf38320683264792b6dd94fa345ab2126
Grigor Azatian <grigorazatian@yahoo.com>
2021-07-12 15:03:26 -0700

    working on a single gameBoard tile, each click of the tile changes it from an X to an O and vice versa

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("testing testing 1 2 3");
+//upon clicking box, populate it with alternating "X" or "O". click it once, it turns to "X", then click it again, it turns to "O", so on. 
+
+//variable that stores an int, if int is odd, fill box with "X", if int is even, fill box with "O"
+
+let gameBoardTile  = document.querySelector(".box");
+
+
+let xOrO = 1;
+gameBoardTile.addEventListener("click", function(){
+    gameBoardTile.style.cssText = "font-size: 350px; text-align: center;";
+    if(xOrO%2===0){
+        gameBoardTile.innerHTML = "O";
+    }else{
+        gameBoardTile.innerHTML = "X";
+    }
+    xOrO++;
+});
+
+
+
+
+
