362d8a265d71e38501bb03e28e9f2b5c170dc2f0
Grigor Azatian <grigorazatian@yahoo.com>
2021-07-12 17:11:46 -0700

    getting closer! 3x3 grid made with each tile either displaying x or o upon click. If tile already has an X/O, clicking it does nothing.

--- script.js
+++ script.js
@@ -5,36 +5,111 @@
 let gameBoardTile1 = document.querySelector(".box1");
 let gameBoardTile2 = document.querySelector(".box2");
 let gameBoardTile3 = document.querySelector(".box3");
+let gameBoardTile4 = document.querySelector(".box4");
+let gameBoardTile5 = document.querySelector(".box5");
+let gameBoardTile6 = document.querySelector(".box6");
+let gameBoardTile7 = document.querySelector(".box7");
+let gameBoardTile8 = document.querySelector(".box8");
+let gameBoardTile9 = document.querySelector(".box9");
 
 let xOrO = 1;
 let fillBox1WithXorO = function(){
-    gameBoardTile1.style.cssText = "font-size: 350px; text-align: center;";
-if(gameBoardTile1.innerHTML === ""){    if(xOrO%2===0){
-        gameBoardTile1.innerHTML = "O";
-    }else{
-        gameBoardTile1.innerHTML = "X";
+    if(gameBoardTile1.innerHTML === ""){    
+        if(xOrO%2===0){
+            gameBoardTile1.innerHTML = "O";
+        }else{
+            gameBoardTile1.innerHTML = "X";
+        }
+        xOrO++;
     }
-    xOrO++;}
 };
 let fillBox2WithXorO = function(){
-    gameBoardTile2.style.cssText = "font-size: 350px; text-align: center;";
-    if(xOrO%2===0){
-        gameBoardTile2.innerHTML = "O";
-    }else{
-        gameBoardTile2.innerHTML = "X";
+    if(gameBoardTile2.innerHTML === ""){    
+        if(xOrO%2===0){
+            gameBoardTile2.innerHTML = "O";
+        }else{
+            gameBoardTile2.innerHTML = "X";
+        }
+        xOrO++;
     }
-    xOrO++;
 };
 let fillBox3WithXorO = function(){
-    gameBoardTile3.style.cssText = "font-size: 350px; text-align: center;";
-    if(xOrO%2===0){
-        gameBoardTile3.innerHTML = "O";
-    }else{
-        gameBoardTile3.innerHTML = "X";
+    if(gameBoardTile3.innerHTML === ""){    
+        if(xOrO%2===0){
+            gameBoardTile3.innerHTML = "O";
+        }else{
+            gameBoardTile3.innerHTML = "X";
+        }
+        xOrO++;
     }
-    xOrO++;
+};
+let fillBox4WithXorO = function(){
+    if(gameBoardTile4.innerHTML === ""){    
+        if(xOrO%2===0){
+            gameBoardTile4.innerHTML = "O";
+        }else{
+            gameBoardTile4.innerHTML = "X";
+        }
+        xOrO++;
+    }
+};
+let fillBox5WithXorO = function(){
+    if(gameBoardTile5.innerHTML === ""){    
+        if(xOrO%2===0){
+            gameBoardTile5.innerHTML = "O";
+        }else{
+            gameBoardTile5.innerHTML = "X";
+        }
+        xOrO++;
+    }
+};
+let fillBox6WithXorO = function(){
+    if(gameBoardTile6.innerHTML === ""){    
+        if(xOrO%2===0){
+            gameBoardTile6.innerHTML = "O";
+        }else{
+            gameBoardTile6.innerHTML = "X";
+        }
+        xOrO++;
+    }
+};
+let fillBox7WithXorO = function(){
+    if(gameBoardTile7.innerHTML === ""){    
+        if(xOrO%2===0){
+            gameBoardTile7.innerHTML = "O";
+        }else{
+            gameBoardTile7.innerHTML = "X";
+        }
+        xOrO++;
+    }
+};
+let fillBox8WithXorO = function(){
+    if(gameBoardTile8.innerHTML === ""){    
+        if(xOrO%2===0){
+            gameBoardTile8.innerHTML = "O";
+        }else{
+            gameBoardTile8.innerHTML = "X";
+        }
+        xOrO++;
+    }
+};
+let fillBox9WithXorO = function(){
+    if(gameBoardTile9.innerHTML === ""){    
+        if(xOrO%2===0){
+            gameBoardTile9.innerHTML = "O";
+        }else{
+            gameBoardTile9.innerHTML = "X";
+        }
+        xOrO++;
+    }
 };
 
 gameBoardTile1.addEventListener("click", fillBox1WithXorO);
 gameBoardTile2.addEventListener("click", fillBox2WithXorO);
 gameBoardTile3.addEventListener("click", fillBox3WithXorO);
+gameBoardTile4.addEventListener("click", fillBox4WithXorO);
+gameBoardTile5.addEventListener("click", fillBox5WithXorO);
+gameBoardTile6.addEventListener("click", fillBox6WithXorO);
+gameBoardTile7.addEventListener("click", fillBox7WithXorO);
+gameBoardTile8.addEventListener("click", fillBox8WithXorO);
+gameBoardTile9.addEventListener("click", fillBox9WithXorO);
