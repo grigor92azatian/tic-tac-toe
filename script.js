7b862e01a280759aeec36372a51d37233d52258d
Grigor Azatian <grigorazatian@yahoo.com>
2021-07-12 22:58:47 -0700

    reduced the repetitiveness of the js code

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 //variable that stores an int, if int is odd, fill box with "X", if int is even, fill box with "O"
 
+
 let gameBoardTile1 = document.querySelector(".box1");
 let gameBoardTile2 = document.querySelector(".box2");
 let gameBoardTile3 = document.querySelector(".box3");
@@ -22,6 +23,7 @@ let fillBox1WithXorO = function(){
         }
         xOrO++;
     }
+    checkGame();
 };
 let fillBox2WithXorO = function(){
     if(gameBoardTile2.innerHTML === ""){    
@@ -32,6 +34,7 @@ let fillBox2WithXorO = function(){
         }
         xOrO++;
     }
+    checkGame();
 };
 let fillBox3WithXorO = function(){
     if(gameBoardTile3.innerHTML === ""){    
@@ -42,6 +45,7 @@ let fillBox3WithXorO = function(){
         }
         xOrO++;
     }
+    checkGame();
 };
 let fillBox4WithXorO = function(){
     if(gameBoardTile4.innerHTML === ""){    
@@ -52,6 +56,7 @@ let fillBox4WithXorO = function(){
         }
         xOrO++;
     }
+    checkGame();
 };
 let fillBox5WithXorO = function(){
     if(gameBoardTile5.innerHTML === ""){    
@@ -62,6 +67,7 @@ let fillBox5WithXorO = function(){
         }
         xOrO++;
     }
+    checkGame();
 };
 let fillBox6WithXorO = function(){
     if(gameBoardTile6.innerHTML === ""){    
@@ -72,6 +78,7 @@ let fillBox6WithXorO = function(){
         }
         xOrO++;
     }
+    checkGame();
 };
 let fillBox7WithXorO = function(){
     if(gameBoardTile7.innerHTML === ""){    
@@ -82,6 +89,7 @@ let fillBox7WithXorO = function(){
         }
         xOrO++;
     }
+    checkGame();
 };
 let fillBox8WithXorO = function(){
     if(gameBoardTile8.innerHTML === ""){    
@@ -92,6 +100,7 @@ let fillBox8WithXorO = function(){
         }
         xOrO++;
     }
+    checkGame();
 };
 let fillBox9WithXorO = function(){
     if(gameBoardTile9.innerHTML === ""){    
@@ -102,6 +111,7 @@ let fillBox9WithXorO = function(){
         }
         xOrO++;
     }
+    checkGame();
 };
 
 gameBoardTile1.addEventListener("click", fillBox1WithXorO);
@@ -113,3 +123,93 @@ gameBoardTile6.addEventListener("click", fillBox6WithXorO);
 gameBoardTile7.addEventListener("click", fillBox7WithXorO);
 gameBoardTile8.addEventListener("click", fillBox8WithXorO);
 gameBoardTile9.addEventListener("click", fillBox9WithXorO);
+
+let checkGame = function(){
+    if(gameBoardTile1.innerHTML==="X"&&gameBoardTile2.innerHTML==="X"&&gameBoardTile3.innerHTML==="X"){
+        gameBoardTile1.style.color = "red";
+        gameBoardTile2.style.color = "red";
+        gameBoardTile3.style.color = "red";
+    }
+    if(gameBoardTile1.innerHTML==="O"&&gameBoardTile2.innerHTML==="O"&&gameBoardTile3.innerHTML==="O"){
+        gameBoardTile1.style.color = "red";
+        gameBoardTile2.style.color = "red";
+        gameBoardTile3.style.color = "red";
+    }
+
+    if(gameBoardTile4.innerHTML==="X"&&gameBoardTile5.innerHTML==="X"&&gameBoardTile6.innerHTML==="X"){
+        gameBoardTile4.style.color = "red";
+        gameBoardTile5.style.color = "red";
+        gameBoardTile6.style.color = "red";
+    }
+    if(gameBoardTile4.innerHTML==="O"&&gameBoardTile5.innerHTML==="O"&&gameBoardTile6.innerHTML==="O"){
+        gameBoardTile4.style.color = "red";
+        gameBoardTile5.style.color = "red";
+        gameBoardTile6.style.color = "red";
+    }
+
+    if(gameBoardTile7.innerHTML==="X"&&gameBoardTile8.innerHTML==="X"&&gameBoardTile9.innerHTML==="X"){
+        gameBoardTile7.style.color = "red";
+        gameBoardTile8.style.color = "red";
+        gameBoardTile9.style.color = "red";
+    }
+    if(gameBoardTile6.innerHTML==="O"&&gameBoardTile7.innerHTML==="O"&&gameBoardTile8.innerHTML==="O"){
+        gameBoardTile7.style.color = "red";
+        gameBoardTile8.style.color = "red";
+        gameBoardTile9.style.color = "red";
+    }
+
+    if(gameBoardTile1.innerHTML==="X"&&gameBoardTile4.innerHTML==="X"&&gameBoardTile7.innerHTML==="X"){
+        gameBoardTile1.style.color = "red";
+        gameBoardTile4.style.color = "red";
+        gameBoardTile7.style.color = "red";
+    }
+    if(gameBoardTile1.innerHTML==="O"&&gameBoardTile4.innerHTML==="O"&&gameBoardTile7.innerHTML==="O"){
+        gameBoardTile1.style.color = "red";
+        gameBoardTile4.style.color = "red";
+        gameBoardTile7.style.color = "red";
+    }
+
+    if(gameBoardTile2.innerHTML==="X"&&gameBoardTile5.innerHTML==="X"&&gameBoardTile8.innerHTML==="X"){
+        gameBoardTile2.style.color = "red";
+        gameBoardTile5.style.color = "red";
+        gameBoardTile8.style.color = "red";
+    }
+    if(gameBoardTile2.innerHTML==="O"&&gameBoardTile5.innerHTML==="O"&&gameBoardTile8.innerHTML==="O"){
+        gameBoardTile2.style.color = "red";
+        gameBoardTile5.style.color = "red";
+        gameBoardTile8.style.color = "red";
+    }
+
+    if(gameBoardTile3.innerHTML==="X"&&gameBoardTile6.innerHTML==="X"&&gameBoardTile9.innerHTML==="X"){
+        gameBoardTile3.style.color = "red";
+        gameBoardTile6.style.color = "red";
+        gameBoardTile9.style.color = "red";
+    }
+    if(gameBoardTile3.innerHTML==="O"&&gameBoardTile6.innerHTML==="O"&&gameBoardTile9.innerHTML==="O"){
+        gameBoardTile3.style.color = "red";
+        gameBoardTile6.style.color = "red";
+        gameBoardTile9.style.color = "red";
+    }
+
+    if(gameBoardTile1.innerHTML==="X"&&gameBoardTile5.innerHTML==="X"&&gameBoardTile9.innerHTML==="X"){
+        gameBoardTile1.style.color = "red";
+        gameBoardTile5.style.color = "red";
+        gameBoardTile9.style.color = "red";
+    }
+    if(gameBoardTile1.innerHTML==="O"&&gameBoardTile5.innerHTML==="O"&&gameBoardTile9.innerHTML==="O"){
+        gameBoardTile1.style.color = "red";
+        gameBoardTile5.style.color = "red";
+        gameBoardTile9.style.color = "red";
+    }
+
+    if(gameBoardTile3.innerHTML==="X"&&gameBoardTile5.innerHTML==="X"&&gameBoardTile7.innerHTML==="X"){
+        gameBoardTile3.style.color = "red";
+        gameBoardTile5.style.color = "red";
+        gameBoardTile7.style.color = "red";
+    }
+    if(gameBoardTile3.innerHTML==="O"&&gameBoardTile5.innerHTML==="O"&&gameBoardTile7.innerHTML==="O"){
+        gameBoardTile3.style.color = "red";
+        gameBoardTile5.style.color = "red";
+        gameBoardTile7.style.color = "red";
+    }
+}
---
+++ script2.js
@@ -0,0 +1,54 @@
+let gameBoardArray = [];                                            //will store our 9 game board tiles
+let gameBoardContainer = document.querySelector(".gameBoard");   //this points to our gameBoard container hardcoded in the html
+let xOrO = 1;                                                   //alternates X and O on gameboard clicks
+
+for(let i=0;i<9;i++){                                              
+    let tile = document.createElement("div");                                                                           //create new div element
+    tile.setAttribute("class", "box"+i);                                                                                //gives each div a unique class name
+    tile.style.cssText = "font-size: 170px; text-align: center; width: 200px; height: 200px; border: 2px solid black;"  //styles each div
+    tile.addEventListener("click", function(){                                                                          //add click event listener to each div
+        if(tile.innerHTML === ""){    
+            if(xOrO%2===0){
+                tile.innerHTML = "O";
+            }else{
+                tile.innerHTML = "X";
+            }
+            xOrO++;
+        }
+        checkGame();
+    });
+    gameBoardArray.push(tile);                                                                                          //push each div onto array for storage
+    gameBoardContainer.appendChild(tile);                                                                               //append all 9 divs to gameBoard Container hardcoded into html
+}
+
+let changeToRed = function(tile1,tile2,tile3){
+    tile1.style.color = "red";
+    tile2.style.color = "red";
+    tile3.style.color = "red";
+}
+let checkGame = function(){
+    if((gameBoardArray[0].innerHTML==="X"&&gameBoardArray[1].innerHTML==="X"&&gameBoardArray[2].innerHTML==="X")||(gameBoardArray[0].innerHTML==="O"&&gameBoardArray[1].innerHTML==="O"&&gameBoardArray[2].innerHTML==="O")){
+        changeToRed(gameBoardArray[0], gameBoardArray[1], gameBoardArray[2]);
+    }
+    if((gameBoardArray[3].innerHTML==="X"&&gameBoardArray[4].innerHTML==="X"&&gameBoardArray[5].innerHTML==="X")||(gameBoardArray[3].innerHTML==="O"&&gameBoardArray[4].innerHTML==="O"&&gameBoardArray[5].innerHTML==="O")){
+        changeToRed(gameBoardArray[3], gameBoardArray[4], gameBoardArray[5]);
+    }
+    if((gameBoardArray[6].innerHTML==="X"&&gameBoardArray[7].innerHTML==="X"&&gameBoardArray[8].innerHTML==="X")||(gameBoardArray[6].innerHTML==="O"&&gameBoardArray[7].innerHTML==="O"&&gameBoardArray[8].innerHTML==="O")){
+        changeToRed(gameBoardArray[6], gameBoardArray[7], gameBoardArray[8]);
+    }
+    if((gameBoardArray[0].innerHTML==="X"&&gameBoardArray[3].innerHTML==="X"&&gameBoardArray[6].innerHTML==="X")||(gameBoardArray[0].innerHTML==="O"&&gameBoardArray[3].innerHTML==="O"&&gameBoardArray[6].innerHTML==="O")){
+        changeToRed(gameBoardArray[0], gameBoardArray[3], gameBoardArray[6]);
+    }
+    if((gameBoardArray[1].innerHTML==="X"&&gameBoardArray[4].innerHTML==="X"&&gameBoardArray[7].innerHTML==="X")||(gameBoardArray[1].innerHTML==="O"&&gameBoardArray[4].innerHTML==="O"&&gameBoardArray[7].innerHTML==="O")){
+        changeToRed(gameBoardArray[1], gameBoardArray[4], gameBoardArray[7]);
+    }
+    if((gameBoardArray[2].innerHTML==="X"&&gameBoardArray[5].innerHTML==="X"&&gameBoardArray[8].innerHTML==="X")||(gameBoardArray[2].innerHTML==="O"&&gameBoardArray[5].innerHTML==="O"&&gameBoardArray[8].innerHTML==="O")){
+        changeToRed(gameBoardArray[2], gameBoardArray[5], gameBoardArray[8]);
+    }
+    if((gameBoardArray[0].innerHTML==="X"&&gameBoardArray[4].innerHTML==="X"&&gameBoardArray[8].innerHTML==="X")||(gameBoardArray[0].innerHTML==="O"&&gameBoardArray[4].innerHTML==="O"&&gameBoardArray[8].innerHTML==="O")){
+        changeToRed(gameBoardArray[0], gameBoardArray[4], gameBoardArray[8]);
+    }
+    if((gameBoardArray[2].innerHTML==="X"&&gameBoardArray[4].innerHTML==="X"&&gameBoardArray[6].innerHTML==="X")||(gameBoardArray[2].innerHTML==="O"&&gameBoardArray[4].innerHTML==="O"&&gameBoardArray[6].innerHTML==="O")){
+        changeToRed(gameBoardArray[2], gameBoardArray[4], gameBoardArray[6]);
+    }
+}
