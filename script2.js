d64ccaccb16f162b4e09a80bffeb3dfbb132bfe4
Grigor Azatian <grigorazatian@yahoo.com>
2021-07-15 18:43:34 -0700

    working on new round button

--- script2.js
+++ script2.js
@@ -7,40 +7,40 @@ let gameBoardModule = function(){
     let totalNumOfMoves = 0;                                        //this keeps track of how many moves have been made by both X and O together, once this number hits 5
                                                                     //its time to check if there are any winning combos
     
-    for(let i=0;i<9;i++){                                              
-        let tile = document.createElement("div");                                                                           //create new div element
-        tile.setAttribute("class", "box"+i);                                                                                //gives each div a unique class name
-        tile.style.cssText = "font-size: 170px; text-align: center; width: 200px; height: 200px;";                          //styles each div
-        if(i>=3&&i<=8){                                                                                               //give tiles their unique borders
-            tile.style.borderTop = "2px solid black";
-        }
-        if(i<=5){
-            tile.style.borderBottom = "2px solid black";
-        }
-        if(i%3!=0){
-            tile.style.borderLeft = "2px solid black";
-        }
-        if(i==0||i==1||i==3||i==4||i==6||i==7){
-            tile.style.borderRight = "2px solid black";
-        }                
-        tile.addEventListener("click", function(){                                                                          //add click event listener to each div
-            if(tile.innerHTML === ""){    
-                if(xOrO%2===0){
-                    tile.innerHTML = "O";
-                }else{
-                    tile.innerHTML = "X";
+        for(let i=0;i<9;i++){                                              
+            let tile = document.createElement("div");                                                                           //create new div element
+            tile.setAttribute("class", "box"+i);                                                                                //gives each div a unique class name
+            tile.style.cssText = "font-size: 170px; text-align: center; width: 200px; height: 200px;";                          //styles each div
+            if(i>=3&&i<=8){                                                                                               //give tiles their unique borders
+                tile.style.borderTop = "2px solid black";
+            }
+            if(i<=5){
+                tile.style.borderBottom = "2px solid black";
+            }
+            if(i%3!=0){
+                tile.style.borderLeft = "2px solid black";
+            }
+            if(i==0||i==1||i==3||i==4||i==6||i==7){
+                tile.style.borderRight = "2px solid black";
+            }                
+            tile.addEventListener("click", function(){                                                                          //add click event listener to each div
+                if(tile.innerHTML === ""){    
+                    if(xOrO%2===0){
+                        tile.innerHTML = "O";
+                    }else{
+                        tile.innerHTML = "X";
+                    }
+                    xOrO++;
                 }
-                xOrO++;
-            }
-            totalNumOfMoves++;                                                                                              //each click will increase this by 1, once it reaches 5, its time to start checking if the game has been won
-            if(totalNumOfMoves>=5){
-                checkGame();    
-            }
-        });
-        gameBoardArray.push(tile);                                                                                          //push each div onto array for storage
-        gameBoardContainer.appendChild(tile);                                                                               //append all 9 divs to gameBoard Container hardcoded into html
-    }
-    
+                totalNumOfMoves++;                                                                                              //each click will increase this by 1, once it reaches 5, its time to start checking if the game has been won
+                if(totalNumOfMoves>=5){
+                    checkGame();    
+                }
+            });
+            gameBoardArray.push(tile);                                                                                          //push each div onto array for storage
+            gameBoardContainer.appendChild(tile);                                                                               //append all 9 divs to gameBoard Container hardcoded into html
+        }        
+
     let changeToRed = function(tile1,tile2,tile3){              //use this function to change the winning 3 tiles into red font
         tile1.style.color = "red";
         tile2.style.color = "red";
@@ -91,6 +91,14 @@ let gameBoardModule = function(){
             displayWinnerOfRound();
         }
     }
+    let clearGameBoard = function(){
+        gameBoardArray.forEach(box => {
+            box.innerHTML = "";
+        });
+        xOrO = 1;
+        totalNumOfMoves = 0;
+    }
+
 };
 
 
@@ -175,13 +183,7 @@ let displayWinnerOfRound = function(playerPiece){
 
 //-----------------------------------------------------------------------------RESET GAME AFTER FIRST ROUND----------------------------------------------------------------------------
 //button to clear the gameboard and continue on with another round while keeping the current scores
-let clearGameBoard = function(){
-    gameBoardArray.forEach(box => {
-        box.innerHTML = "";
-    });
-    xOrO = 1;
-    totalNumOfMoves = 0;
-}
+
 //button to clear the scores and start a new game while keeping the same player names
 
 //button to start from scratch where player names need to be entered again (same effect as refresh button)
